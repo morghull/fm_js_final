1ec97201a355eb0bea552b71abc771308b487047
morghull <morghull@gmail.com>
2021-11-05 15:58:29 +0200

    scale createElement function
    to increase it's usability

--- assets/js/createElement.js
+++ assets/js/createElement.js
@@ -1,18 +1,28 @@
+/**
+ *
+ * @param {string} type
+ * @param {object} options
+ * @param {string[]} options.classNames
+ * @param {object} options.attributes
+ * @param {object} options.events
+ * @param  {...Node[]} children
+ * @returns
+ */
 function createElement(
-    type,
-    { classNames = [], attributes = null, events = null } = {},
-    ...children
-  ) {
-    const elem = document.createElement(type);
-    if (classNames) elem.classList.add(...classNames);
-    if (attributes)
-      for (const attr of attributes) {
-        elem.setAttribute(attr[0], attr[1]);
-      }
-    elem.append(...children);
-    if (events)
-      for (const event of events) {
-        elem.addEventListener(event[0], event[1]);
-      }
-    return elem;
-  }
+  type = div,
+  { classNames = [], attributes = {}, events = {} } = {},
+  ...children
+) {
+  const elem = document.createElement(type);
+  if (classNames) elem.classList.add(...classNames);
+  if (attributes)
+    for (const [attrName, attrValue] of Object.entries(attributes)) {
+      elem.setAttribute(attrName, attrValue);
+    }
+  if (events)
+    for (const [eventType, eventHandler] of Object.entries(events)) {
+      elem.addEventListener(eventType, eventHandler);
+    }
+  elem.append(...children);
+  return elem;
+}
